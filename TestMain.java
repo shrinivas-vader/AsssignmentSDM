4e62365049a7c4c9ede229db53e9a3b1df06fddd
Shrinivas <shrinivasvader@gmail.com>
2022-07-04 15:33:34 +0530

    Added java demo application files

---
+++ TestMain.java
@@ -0,0 +1,14 @@
+package com.app.assignment1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+	@Test
+	
+	public void testInputIsEven(){
+		assertTrue(Main.checkIfInputIsAnEvenNumber(22)); 
+		//Assertion
+		}
+}
